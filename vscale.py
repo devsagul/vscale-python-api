cb9670eb8cf3fe0c7929134997f52f4ac2ef1823
devsagul <dev.sagul@gmail.com>
2018-01-20 16:06:58 +0300

    added get_scalets function

--- vscale.py
+++ vscale.py
@@ -2,9 +2,9 @@ import requests
 
 
 """
-Function account performs GET-request at https://api.vscale.io/v1/account,
+Function account performs a GET-request at https://api.vscale.io/v1/account,
 returns full information on user: name, activation date, email.
-The only parameter is token that has to be provided as a str object
+The only parameter is token that has to be provided as a str object.
 """
 
 
@@ -15,8 +15,13 @@ def account(token):
 
 
 """
-
+Function get_scalets performs a GET-request at
+https://api.vscale.io/v1/scalets, returns information on servers.
+The only parameter is token that has to be provided as a str object.
 """
 
 
-def
+def get_scalets(token):
+    return requests.get("https://api.vscale.io/v1/scalets",
+                        headers={"X-Token": token}
+                        )
