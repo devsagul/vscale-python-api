d70c3271cb5f77a7b68340849c5e7006248e0996
devsagul <dev.sagul@gmail.com>
2018-01-20 16:00:10 +0300

    renamed accounts into account

--- vscale.py
+++ vscale.py
@@ -2,13 +2,21 @@ import requests
 
 
 """
-Function accounts performs GET-request at https://api.vscale.io/v1/account,
+Function account performs GET-request at https://api.vscale.io/v1/account,
 returns full information on user: name, activation date, email.
 The only parameter is token that has to be provided as a str object
 """
 
 
-def accounts(token):
+def account(token):
     return requests.get("https://api.vscale.io/v1/account",
                         headers={"X-Token": token}
                         )
+
+
+"""
+
+"""
+
+
+def
